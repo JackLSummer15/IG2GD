36fc6ba5be46e217a32393ba8f42ace85668e7af
JackLSummer15 <62530870+JackLSummer15@users.noreply.github.com>
2022-03-29 19:23:53 -0400

    bkg colors, pits fixed, offsets fixed

--- ig2gd.py
+++ ig2gd.py
@@ -44,26 +44,53 @@ for level in os.listdir('input'):
 
     elif level.endswith('.dat'): #impossible game 1
         with open('input/'+level,'rb') as l:
-            l.read(6)
-            objectcount=struct.pack('>c',l.read(1))[0]
+            l.read(5)
+            objectcount=struct.unpack('>h',l.read(2))[0]
             print('adding '+str(objectcount)+' objects...')
             for object in range(objectcount):
                 objectid=struct.pack('>c',l.read(1))[0]
                 l.read(1)
                 l.read(1)
-                unknowncount=struct.pack('>c',l.read(1))[0] #unknown count???
-                width=struct.pack('>c',l.read(1))[0]#+330
+                width=struct.unpack('>h',l.read(2))[0]#+330
                 l.read(1)
                 l.read(1)
-                l.read(1)
-                unknowncount=unknowncount-2
-                multiply=unknowncount*255
-                height=struct.pack('>c',l.read(1))[0]+15
+                height=struct.unpack('>h',l.read(2))[0]+15
                 if objectid==1: #spike
-                    output.write('1,8,2,'+str(width+multiply)+',3,'+str(height)+';')
+                    output.write('1,8,2,'+str(width)+',3,'+str(height)+';')
                 elif objectid==0: #block
-                    output.write('1,1,2,'+str(width+multiply)+',3,'+str(height)+';')
+                    output.write('1,1,2,'+str(width)+',3,'+str(height)+';')
                 elif objectid==2: #pit
-                    output.write('1,1715,2,'+str(width+multiply)+',3,2.5;')
+                    for pitwidth in range(width,height,30):
+                        output.write('1,1715,2,'+str(pitwidth)+',3,2.5;')
+            l.read(2)
+            levelend=struct.unpack('>h',l.read(2))[0]
+            l.read(3)
+            bkgcount=struct.pack('>c',l.read(1))[0]
+            print(str(bkgcount)+' background triggers...')
+            for bkg in range(bkgcount):
+                l.read(2)
+                bkgwidth=struct.unpack('>h',l.read(2))[0]
+                l.read(4)
+                bkgcolor=struct.pack('>c',l.read(1))[0]
+                if bkgcolor==0: #blue
+                    output.write('1,29,2,'+str(bkgwidth)+',3,-15,36,1,7,0,8,102,9,255,10,0.5,35,1,23,1000;')
+                    output.write('1,30,2,'+str(bkgwidth)+',3,-45,36,1,7,0,8,102,9,255,10,0.5,35,1,23,1001;')
+                elif bkgcolor==1: #yellow
+                    output.write('1,29,2,'+str(bkgwidth)+',3,-15,36,1,7,255,8,255,9,0,10,0.5,35,1,23,1000;')
+                    output.write('1,30,2,'+str(bkgwidth)+',3,-45,36,1,7,255,8,255,9,0,10,0.5,35,1,23,1001;')
+                elif bkgcolor==2: #green
+                    output.write('1,29,2,'+str(bkgwidth)+',3,-15,36,1,7,0,8,255,9,0,10,0.5,35,1,23,1000;')
+                    output.write('1,30,2,'+str(bkgwidth)+',3,-45,36,1,7,0,8,255,9,0,10,0.5,35,1,23,1001;')
+                elif bkgcolor==3: #violet
+                    output.write('1,29,2,'+str(bkgwidth)+',3,-15,36,1,7,177,8,0,9,252,10,0.5,35,1,23,1000;')
+                    output.write('1,30,2,'+str(bkgwidth)+',3,-45,36,1,7,177,8,0,9,252,10,0.5,35,1,23,1001;')
+                elif bkgcolor==4: #pink
+                    output.write('1,29,2,'+str(bkgwidth)+',3,-15,36,1,7,255,8,0,9,191,10,0.5,35,1,23,1000;')
+                    output.write('1,30,2,'+str(bkgwidth)+',3,-45,36,1,7,255,8,0,9,191,10,0.5,35,1,23,1001;')
+                elif bkgcolor==5: #black
+                    output.write('1,29,2,'+str(bkgwidth)+',3,-15,36,1,7,0,8,0,9,0,10,0.5,35,1,23,1000;')
+                    output.write('1,30,2,'+str(bkgwidth)+',3,-45,36,1,7,0,8,0,9,0,10,0.5,35,1,23,1001;')
+            #1,29,2,width,3,height,36,1,7,red,8,green,9,blue,10,duration,35,1,23,1000; #bkg trigger
+            #1,30,2,width,3,height,36,1,7,red,8,green,9,blue,10,duration,35,1,23,1001; #ground trigger
 
     output.close()
