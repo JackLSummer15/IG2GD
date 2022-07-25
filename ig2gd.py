bc8da5945b7d36f87b665ee274b0983f716b16be
JackLSummer15 <62530870+JackLSummer15@users.noreply.github.com>
2022-07-25 19:26:30 -0400

    zoom, timewarp and offset triggers for ig1

--- ig2gd.py
+++ ig2gd.py
@@ -9,10 +9,32 @@ try:
 except:
     pass
 
-levelplaceholder='kS38,1_40_2_125_3_255_11_255_12_255_13_255_4_-1_6_1000_7_1_15_1_18_0_8_1|1_0_2_102_3_255_11_255_12_255_13_255_4_-1_6_1001_7_1_15_1_18_0_8_1|1_0_2_102_3_255_11_255_12_255_13_255_4_-1_6_1009_7_1_15_1_18_0_8_1|1_255_2_255_3_255_11_255_12_255_13_255_4_-1_6_1002_5_1_7_1_15_1_18_0_8_1|1_125_2_0_3_255_11_255_12_255_13_255_4_-1_6_1005_5_1_7_1_15_1_18_0_8_1|1_125_2_0_3_255_11_255_12_255_13_255_4_-1_6_1006_5_1_7_1_15_1_18_0_8_1|,kA13,0,kA15,0,kA16,0,kA14,,kA6,0,kA7,0,kA17,0,kA18,0,kS39,0,kA2,0,kA3,0,kA8,0,kA4,0,kA9,0,kA10,0,kA11,0;'
+#level info
+#kA2=gamemode(0=cude,1=ship,2=ball,3=ufo,4=wave,5=robot,6=spider,7=swing)
+#kA3=mini
+#kA4=speed(1,-1,2,3,4)
+#kA6=background
+#kA7=ground
+#kA8=dual
+#kA10=2player
+#kA13=songoffset
+#kA15=fadein
+#kA16=fadeout
+#kA17=alternateline
+#kA18=font
+
+#2.2 object ids:
+#1917=reverse trigger
+#1935=timewarp trigger
+
+levelplaceholder='kS38,1_40_2_125_3_255_11_255_12_255_13_255_4_-1_6_1000_7_1_15_1_18_0_8_1|1_0_2_102_3_255_11_255_12_255_13_255_4_-1_6_1001_7_1_15_1_18_0_8_1|1_0_2_102_3_255_11_255_12_255_13_255_4_-1_6_1009_7_1_15_1_18_0_8_1|1_255_2_255_3_255_11_255_12_255_13_255_4_-1_6_1002_5_1_7_1_15_1_18_0_8_1|1_40_2_125_3_255_11_255_12_255_13_255_4_-1_6_1013_7_1_15_1_18_0_8_1|1_40_2_125_3_255_11_255_12_255_13_255_4_-1_6_1014_7_1_15_1_18_0_8_1|1_125_2_0_3_255_11_255_12_255_13_255_4_-1_6_1005_5_1_7_1_15_1_18_0_8_1|1_125_2_0_3_255_11_255_12_255_13_255_4_-1_6_1006_5_1_7_1_15_1_18_0_8_1|,kA13,0,kA15,1,kA16,1,kA14,,kA6,1,kA7,0,kA25,0,kA17,0,kA18,0,kS39,0,kA2,0,kA3,0,kA8,0,kA4,0,kA9,0,kA10,0,kA22,0,kA23,0,kA24,0,kA27,0,kA19,0,kA26,0,kA20,0,kA21,0,kA11,0;'
+
+#impossible game 1 options
+ig1options='1,1913,2,-29,3,-75,155,1,36,1,109,2,10,0,30,0,85,2;1,1916,2,-29,3,-135,155,1,36,1,28,45,29,0,10,0,30,0,85,2;1,1935,2,-29,3,-105,155,1,13,1,36,1,120,1.15;' #2.2 triggers
 
 #teleport objects because the impossible game 2 uses blocks for the ground
 teleport='1,747,2,15,3,15,32,0.5,54,1105;'
+backwardsportal='1,1917,2,-29,3,45,32,2,13,1,11,1,36,1;1,10,2,-29,3,45,36,1;'
 cameraposition='0;1,1913,2,-29,3,45,36,1,109,4,10,0;1,1916,2,-29,3,75,36,1,28,-150,29,75,10,0,30,0,85,2;'
 
 for level in os.listdir('input'):
@@ -21,6 +43,7 @@ for level in os.listdir('input'):
     output=open('output/'+filename+'.plist','w')
     output.write(levelplaceholder)
     if level.endswith('.json'): #impossible game 2
+        
         ig2lvl=json.load(open('input/'+level))
         blocks=ig2lvl[2][0][3][1]
         spikes=ig2lvl[2][0][3][2]
@@ -45,12 +68,27 @@ for level in os.listdir('input'):
             height=spike[1]+1005
             angle=spike[2]*90
             output.write('1,8,2,'+str(width)+',3,'+str(height)+',6,'+str(angle)+';')
+        print('adding '+str(len(platforms))+' platforms...')
+        for platform in platforms:
+            width=platform[0]
+            height=platform[1]+1005
+            output.write('1,40,2,'+str(width)+',3,'+str(height)+',32,1.75;')
         print('adding '+str(len(rev_triggers))+' reverse triggers...')
         for rev_trigger in rev_triggers:
             width=rev_trigger[0]
             height=rev_trigger[1]+1005
             #output.write('1,50,2,'+str(width)+',3,'+str(height)+';') #finding a object id for reverse trigger...
-            output.write('1,1614,2,'+str(width)+',3,'+str(height)+'32,2,13,1,11,1,36,1;1,10,2,'+str(width)+',3,'+str(height)+',36,1;')
+            output.write('1,1917,2,'+str(width)+',3,'+str(height)+'32,2,13,1,11,1,36,1;1,10,2,'+str(width)+',3,'+str(height)+',36,1;') #finding a object id for reverse trigger...
+        print('adding '+str(len(saws))+' saws...')
+        for saw in saws:
+            width=saw[0]
+            height=saw[1]+1005
+            output.write('1,50,2,'+str(width)+',3,'+str(height)+';')
+        print('adding '+str(len(enemies))+' enemies...')
+        for enemy in enemies:
+            width=enemy[0]
+            height=enemy[1]+1005
+            output.write('1,50,2,'+str(width)+',3,'+str(height)+';')
         print('adding '+str(len(collectables))+' collectables...')
         for collectable in collectables:
             width=collectable[0]
@@ -59,6 +97,7 @@ for level in os.listdir('input'):
         output.write(teleport+cameraposition)
 
     elif level.endswith('.dat'): #impossible game 1
+        output.write(ig1options)
         with open('input/'+level,'rb') as l:
             l.read(5)
             objectcount=struct.unpack('>h',l.read(2))[0]
